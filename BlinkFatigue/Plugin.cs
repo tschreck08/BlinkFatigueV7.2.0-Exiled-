dcbb8bdd679ab41ca047c555de3f6864b48e3056
tschreck08 <61746994+tschreck08@users.noreply.github.com>
2023-07-29 14:39:07 -0500

    Plugin.cs

--- BlinkFatigue/Plugin.cs
+++ BlinkFatigue/Plugin.cs
@@ -11,8 +11,8 @@ namespace BlinkFatigue
         public override string Author { get; } = "Galaxy119";
         public override string Name { get; } = "BlinkFatigue";
         public override string Prefix { get; } = "BlinkFatigue";
-        public override Version Version { get; } = new Version(1, 0, 0);
-        public override Version RequiredExiledVersion { get; } = new Version(2, 1, 9);
+        public override Version Version { get; } = new Version(7, 2, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(7, 2, 0);
 
         public static Plugin Singleton;
         
